fce5e2ab0cc0f6e74fcbe7ad5cf4ba43cbb83592
unknown <lucas.200061@gmail.com>
2022-07-18 21:56:05 -0300

    dia2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 
 
 import React from 'react'
-
+import hed1 from "../assets/hed1.jpeg"
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className='img1'>
+        <img src={hed1} alt="img" />
+      </div>
+
+
+      <div className='hed-txt'> <h1>Hola soy Lucas Web Developer con preferencia al Back !</h1> </div>
+
+
+    </div>
   )
 }
 
--- src/components/MiNav.jsx
+++ src/components/MiNav.jsx
@@ -1,18 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { Navbar, Container, Nav } from 'react-bootstrap'
-
+import "../styles/MiNav.css"
 function MiNav() {
 
 
     return (
         <nav className='nav1'>
-            <div><h1>Hola!</h1></div>
+            <div className='navt'><h1><a href="#">Lucas</a> </h1></div>
             <ul>
                 <li><a href="#">Me</a></li>
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">Projects</a></li>
             </ul>
-            <div>
+            <div className='socials'>
                 <a href="https://github.com/lucas22-f"><i class="fa-brands fa-github"></i></a>
                 <a href="https://www.linkedin.com/in/lucas-figueroa-3b5743226/"><i class="fa-brands fa-linkedin"></i></a>
                 <a href="https://www.instagram.com/lucas.figueroa22/?theme=dark"><i class="fa-brands fa-instagram"></i></a>
