cb131cc5fc6d8b3cb7b0bcb94a8050311f2060fc
unknown <lucas.200061@gmail.com>
2022-07-19 18:44:37 -0300

    dia3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,17 @@ function Header() {
   return (
     <div className='header'>
       <div className='img1'>
+        
         <img src={hed1} alt="img" />
+
       </div>
 
 
-      <div className='hed-txt'> <h1>Hola soy Lucas Web Developer con preferencia al Back !</h1> </div>
+      <div className='hed-txt'> 
+       
+        <h1>Hola soy Lucas Web Developer con preferencia al Back !</h1>
+
+      </div>
 
 
     </div>
--- src/components/MiNav.jsx
+++ src/components/MiNav.jsx
@@ -1,19 +1,30 @@
+import { useEffect, useState } from "react";
 import "../styles/MiNav.css"
 function MiNav() {
+    const [sideBar, setSideBar] = useState(false);
+
+ const handletoggle = () =>{
+    setSideBar(!sideBar)
+ }
+
+
+
+
 
 
     return (
         <nav className='nav1'>
             <div className='navt'><h1><a href="#">Lucas</a> </h1></div>
-            <ul>
-                <li><a href="#">Me</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Projects</a></li>
+            <ul className={sideBar ? "nav-menu" : "nav-menu active"}>
+                <li><a href="#">Sobre mi</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Proyectos</a></li>
             </ul>
-            <div className='socials'>
-                <a href="https://github.com/lucas22-f"><i class="fa-brands fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/lucas-figueroa-3b5743226/"><i class="fa-brands fa-linkedin"></i></a>
-                <a href="https://www.instagram.com/lucas.figueroa22/?theme=dark"><i class="fa-brands fa-instagram"></i></a>
+            <div className="hmb" ><i onClick={handletoggle} className="fa-solid fa-bars"></i></div>
+            <div className={sideBar ? "socials" : "socials active"}>
+                <a href="https://github.com/lucas22-f"><i className="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/lucas-figueroa-3b5743226/"><i className="fa-brands fa-linkedin"></i></a>
+                <a href="https://www.instagram.com/lucas.figueroa22/?theme=dark"><i className="fa-brands fa-instagram"></i></a>
             </div>
 
         </nav>
